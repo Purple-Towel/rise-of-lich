46227300608925ed3e8479b907669b82291928d2
Purple-Towel <angadgrewal31@gmail.com>
2020-06-05 22:46:30 +0000

    trimmed unused code from App component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,35 +1,16 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-      message: 'Click the button to load data!'
-    }
-  }
-
-  fetchData = () => {
-    axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
-    .then((response) => {
-      // handle success
-      console.log(response.data) // The entire response from the Rails API
-
-      console.log(response.data.message) // Just the message
-      this.setState({
-        message: response.data.message
-      });
-    }) 
   }
 
   render() {
     return (
-      <div className="App" id="phaser-example">
-        <h1>{ this.state.message }</h1>
-        <button onClick={this.fetchData} >
-          Fetch Data
-        </button>        
+      <div className="App">
+        <h1>Play Game!</h1>
+        <div id="phaser-example"/>       
       </div>
     );
   }
