ee702bdc6dca4e5d6383fab269f3f618c1284db6
Purple-Towel <angadgrewal31@gmail.com>
2020-06-06 00:05:29 +0000

    useToggleHook implemented in App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,7 +7,10 @@ import About from './About';
 import { useToggleHook } from "./hooks/useToggleHook"
 
 export default function App() {
+  const ABOUT = "ABOUT";
+  const TUTORIAL = "TUTORIAL";
 
+  const { mode, toggle} = useToggleHook(ABOUT, TUTORIAL);
 
     return (
       <div className="App">
