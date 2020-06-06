155e5695380173fe42e633e2f61671dcd44f3f06
Purple-Towel <angadgrewal31@gmail.com>
2020-06-06 03:52:08 +0000

    test code for nav bar component

---
+++ client/src/components/Nav.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Nav extends Component {
+    render() {
+        return (
+        <button>
+            Test
+        </button>
+        )
+    }
+}
+
+export default Nav;
