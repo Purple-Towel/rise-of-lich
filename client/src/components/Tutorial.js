e22bc9c49b5d7a7350c76c9c61f7107fdc037535
Purple-Towel <angadgrewal31@gmail.com>
2020-06-08 23:00:47 +0000

    added a simple tutorial for movement

--- client/src/components/Tutorial.js
+++ client/src/components/Tutorial.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
+import "./Tutorial.css";
 
 class Tutorial extends Component {
     render() {
         return (
             <div>
                 <h1>Tutorial</h1>
-                <p>Spicy jalapeno bacon ipsum dolor amet buffalo doner pig pork chop beef ribs. Tail jerky pork chuck cupim. Tail fatback shankle ball tip boudin chislic. Meatball cow salami, ham cupim pork loin venison. Short loin turducken bacon pork belly, boudin brisket venison alcatra andouille pig cupim ham ham hock.</p>
+                <div class="grid2x3">
+                    <img src="assets/empty.png"/>
+                    <img src="assets/up_arr.png"/>
+                    <img src="assets/empty.png"/>
+                    <img src="assets/left_arr.png"/>
+                    <img src="assets/down_arr.png"/>
+                    <img src="assets/right_arr.png"/>
+                </div>
+                <p>Movement: You may press any of the above indicated keys to move the character in the corresponding direction. Moving takes Stamina, and once you reach 0 Stamina you will game over.</p>
             </div>
         )
     }
