bc2a6599251930ad027c2ea6da42661aa662f549
rancewcampbell <rancewcampbell@gmail.com>
2020-06-10 12:31:56 -0600

    test

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,14 +1,39 @@
-import React, { Component } from "react";
+import React from 'react';
 
-class About extends Component {
-    render() {
-        return (
-            <div>
-                <h1>About</h1>
-                <p>Spicy jalapeno bacon ipsum dolor amet leberkas shankle strip steak pig brisket turkey, shank chuck ham tail capicola drumstick jowl biltong pastrami. Burgdoggen tail spare ribs chislic brisket prosciutto pork loin porchetta short loin. Jowl fatback meatball chislic corned beef. Pork loin jerky tongue porchetta fatback chislic ham bresaola burgdoggen turducken. Tri-tip sirloin pig, short ribs ground round hamburger rump. Cow shank bresaola, turkey bacon flank pig strip steak tail.</p>  
-            </div>
-        )
-    }
+function About() {
+  const paragraph = `
+  Welcome to Rise of Lich: Revengeance! We are Angad, Diogo, and Rance and we built this
+  game as a final project for the Canadian coding bootcamp known as Lighthouse Labs. Rise of Lich
+  was created using Phaser 3 to drive the game logic with React components outside of the game.
+  This game is a week long effort by the three of us to show how much we have learned over the course
+  of our 12 week bootcamp. We hope you enjoy it!
+  `;
+  return (
+    <div>
+      <h1>About</h1>
+      <p>{paragraph}</p>
+      <ul>
+        <li>
+          <a>
+            <img src="/assets/logo.png"></img>
+          </a>
+        </li>
+      </ul>
+      <div>
+        Icons made by{' '}
+        <a
+          href="https://www.flaticon.com/authors/pixel-perfect"
+          title="Pixel perfect"
+        >
+          Pixel perfect
+        </a>{' '}
+        from{' '}
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default About;
