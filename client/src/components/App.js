7b37f3a6ffedd6bbd190de76f749490102175165
Purple-Towel <angadgrewal31@gmail.com>
2020-06-11 20:45:49 +0000

    reimplemented old logic for tutorial/about toggling as material UI was buggy with app

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 import './App.css';
 import Game from './Game';
-import Navbar from './Navbar';
+//import Navbar from './Navbar';
+import Button from "./Button";
+import About from "./About";
+import Tutorial from "./Tutorial";
+import { useToggleHook } from "./hooks/useToggleHook";
 
 const GameCanvas = styled.div`
   margin-left: auto;
@@ -11,12 +15,19 @@ const GameCanvas = styled.div`
 `;
 
 export default function App() {
+  const ABOUT = "ABOUT";
+  const TUTORIAL = "TUTORIAL";
+
+  const { mode, toggleState } = useToggleHook(ABOUT, TUTORIAL);
+
   return (
     <div className="App">
       <GameCanvas>
         <Game />
       </GameCanvas>
-      <Navbar />
+      <div class="center-div"><Button onClick={toggleState} mode={mode} /></div>
+      {mode === TUTORIAL && (<Tutorial />)}
+      {mode === ABOUT && (<About />)}   
     </div>
   );
 }
