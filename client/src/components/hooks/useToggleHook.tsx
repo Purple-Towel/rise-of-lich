797cea2207b5ed0aad10ffd0d5a37a9def0e5533
Purple-Towel <angadgrewal31@gmail.com>
2020-06-11 20:44:01 +0000

    reimplemented useToggleState as .tsx

---
+++ client/src/components/hooks/useToggleHook.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+//custom hook to toggle modes in our app between 2 states.
+export function useToggleHook(mode1: string, mode2: string) {
+    const [mode, setMode] = useState(mode1);
+
+    const toggleState = () => {
+        if (mode === mode1) {
+            setMode(mode2);
+        }
+        if (mode === mode2) {
+            setMode(mode1);
+        }
+    };
+
+    return { mode, toggleState}
+}
