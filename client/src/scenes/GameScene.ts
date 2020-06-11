0fd4543ac867c3dedf34735463c6302b87ce35ea
DPintoLL <diogosmp@gmail.com>
2020-06-10 23:44:57 -0700

    Added sample soundFX and bg music

--- client/src/scenes/GameScene.ts
+++ client/src/scenes/GameScene.ts
@@ -1,10 +1,10 @@
-import Phaser from 'phaser';
+import Phaser from "phaser";
 // import movesLeft from "../helpers/movesLeft";
-import level1 from '../game_components/levels/level1';
-import level2 from '../game_components/levels/level2';
-import level3 from '../game_components/levels/level3';
-import level4 from '../game_components/levels/level4';
-import level5 from '../game_components/levels/level5';
+import level1 from "../game_components/levels/level1";
+import level2 from "../game_components/levels/level2";
+import level3 from "../game_components/levels/level3";
+import level4 from "../game_components/levels/level4";
+import level5 from "../game_components/levels/level5";
 //import ending from '../game_components/levels/ending';
 
 export default class Game extends Phaser.Scene {
@@ -16,7 +16,7 @@ export default class Game extends Phaser.Scene {
   private spikesAlternating2: Phaser.GameObjects.Sprite[] = [];
   private barriers: Phaser.GameObjects.Sprite[] = [];
   private layer?: Phaser.Tilemaps.StaticTilemapLayer;
-  private facing: 'right' | 'left' = 'right';
+  private facing: "right" | "left" = "right";
   private moves: number = 50;
   private isGameOver: boolean = false;
   private steps = 0;
@@ -26,19 +26,28 @@ export default class Game extends Phaser.Scene {
   // private movesLeft?: movesLeft;
   private levels = [level1, level2, level3, level4, level5];
   private currentLevel: number = 1;
+
+  //Todo: Refactor into an object with each sound as keys
+  private bgMusic!: Phaser.Sound.BaseSound;
+  private boxDragSound!: Phaser.Sound.BaseSound;
+  private wallBumpSound!: Phaser.Sound.BaseSound;
+
   constructor() {
-    super('game');
+    super("game");
   }
 
   preload() {
     this.cursors = this.input.keyboard.createCursorKeys();
+    this.load.audio("bg_music", "assets/audio/eerie-music.ogg");
+    this.load.audio("audio_box_drag", "assets/audio/drag-gravel.ogg");
+    this.load.audio("audio_wall_bump", "assets/audio/wall_bump.ogg");
   }
 
   create(d: { currentLevel: number; steps: number }) {
     const { currentLevel, steps } = d;
     this.currentLevel = currentLevel;
     this.steps = steps;
-    this.facing = 'right';
+    this.facing = "right";
     const map = this.make.tilemap({
       data: this.levels[this.currentLevel - 1].map,
       tileWidth: 16,
@@ -47,44 +56,60 @@ export default class Game extends Phaser.Scene {
 
     this.moves = this.levels[this.currentLevel - 1].moves;
 
-    const tiles = map.addTilesetImage('tiles');
+    const tiles = map.addTilesetImage("tiles");
     this.layer = map.createStaticLayer(0, tiles, 0, 0);
 
     this.barriers = this.layer
-      .createFromTiles(49, 11, { key: 'tiles', frame: 49 })
-      .map(barrier => barrier.setOrigin(0));
+      .createFromTiles(49, 11, { key: "tiles", frame: 49 })
+      .map((barrier) => barrier.setOrigin(0));
 
     this.spikes = this.layer
-      .createFromTiles(777, 11, { key: 'character', frame: 356 })
-      .map(spike => spike.setOrigin(0));
+      .createFromTiles(777, 11, { key: "character", frame: 356 })
+      .map((spike) => spike.setOrigin(0));
 
     this.spikesAlternating1 = this.layer
-      .createFromTiles(778, 11, { key: 'character', frame: 353 })
-      .map(spikeAlternating1 => spikeAlternating1.setOrigin(0));
+      .createFromTiles(778, 11, { key: "character", frame: 353 })
+      .map((spikeAlternating1) => spikeAlternating1.setOrigin(0));
 
     this.spikesAlternating2 = this.layer
-      .createFromTiles(779, 11, { key: 'character', frame: 356 })
-      .map(spikeAlternating1 => spikeAlternating1.setOrigin(0));
+      .createFromTiles(779, 11, { key: "character", frame: 356 })
+      .map((spikeAlternating1) => spikeAlternating1.setOrigin(0));
 
     this.boxes = this.layer
-      .createFromTiles(83, 11, { key: 'tiles', frame: 83 })
-      .map(box => box.setOrigin(0));
+      .createFromTiles(83, 11, { key: "tiles", frame: 83 })
+      .map((box) => box.setOrigin(0));
 
     this.player = this.layer
-      .createFromTiles(400, 11, { key: 'character', frame: 40 })
+      .createFromTiles(400, 11, { key: "character", frame: 40 })
       .pop();
 
     this.player?.setOrigin(0);
     this.createPlayerAnimations();
     this.createSpikeAnimations();
 
-    this.add.image(16, 16, 'hud-icon');
+    this.add.image(16, 16, "hud-icon");
     this.movesText = this.add.text(8, 8, `${this.moves}`, {
       fontSize: "14px",
       fill: "#f00",
     });
     this.movesText.setShadow(1, 1);
     this.stepsText = this.add.text(16, 150, `Steps: ${this.steps}`);
+
+    //-- Audio --
+    //! Declared a config object for tweaking. Most of these are defaults
+    const musicConfig = {
+      mute: false,
+      volume: 0.5,
+      rate: 1,
+      detune: 0,
+      seek: 0,
+      loop: false,
+      delay: 0,
+    };
+    this.bgMusic = this.sound.add("bg_music", musicConfig);
+    this.bgMusic.play();
+    this.boxDragSound = this.sound.add("audio_box_drag", { volume: 0.4 });
+    this.wallBumpSound = this.sound.add("audio_wall_bump", { volume: 0.5 });
   }
 
   update() {
@@ -92,7 +117,7 @@ export default class Game extends Phaser.Scene {
       return;
     }
     if (this.isGameOver === true) {
-      this.scene.start('gameOver', { currentLevel: this.currentLevel });
+      this.scene.start("gameOver", { currentLevel: this.currentLevel });
       this.isGameOver = false;
     }
 
@@ -103,33 +128,33 @@ export default class Game extends Phaser.Scene {
 
     if (justRight) {
       if (!this.player) return;
-      if (this.facing === 'left') {
+      if (this.facing === "left") {
         this.player.toggleFlipX();
-        this.facing = 'right';
+        this.facing = "right";
       }
       // players next coords
       const nx = this.player.x + 24;
       const ny = this.player.y + 8;
-      this.tweenMove(nx, ny, 'x', 'positive');
+      this.tweenMove(nx, ny, "x", "positive");
     } else if (justLeft) {
       if (!this.player) return;
-      if (this.facing === 'right') {
+      if (this.facing === "right") {
         this.player.toggleFlipX();
-        this.facing = 'left';
+        this.facing = "left";
       }
       const nx = this.player.x - 8;
       const ny = this.player.y + 8;
-      this.tweenMove(nx, ny, 'x', 'negative');
+      this.tweenMove(nx, ny, "x", "negative");
     } else if (justDown) {
       if (!this.player) return;
       const nx = this.player.x + 8;
       const ny = this.player.y + 24;
-      this.tweenMove(nx, ny, 'y', 'positive');
+      this.tweenMove(nx, ny, "y", "positive");
     } else if (justUp) {
       if (!this.player) return;
       const nx = this.player.x + 8;
       const ny = this.player.y - 8;
-      this.tweenMove(nx, ny, 'y', 'negative');
+      this.tweenMove(nx, ny, "y", "negative");
     }
   }
 
@@ -137,20 +162,24 @@ export default class Game extends Phaser.Scene {
     x: number,
     y: number,
     axis: string,
-    direction: 'positive' | 'negative'
+    direction: "positive" | "negative"
   ) {
     // check if already tweening, if so, then don't do anything.
     if (this.tweens.isTweening(this.player!)) return undefined;
 
     // if next move has wall escape early
-    if (this.hasObstruction(x, y)) return undefined;
+    if (this.hasObstruction(x, y)) {
+      //TODO: Add little shake when player bumps walls
+      this.wallBumpSound.play();
+      return undefined;
+    }
 
     // if you reach the finishing tile, start the next scene
-    if (this.getTileAt(x, y, 39) && (this.moves >= 2) && !(this.isGameOver)) {
+    if (this.getTileAt(x, y, 39) && this.moves >= 2 && !this.isGameOver) {
       const moves = this.levels[this.currentLevel - 1].moves;
       this.currentLevel++;
       setTimeout(() => {
-        this.scene.start('transition', {
+        this.scene.start("transition", {
           currentLevel: this.currentLevel,
           stepsTaken: this.steps,
         });
@@ -158,8 +187,8 @@ export default class Game extends Phaser.Scene {
     }
 
     const directionXY = {
-      positive: '+=16',
-      negative: '-=16',
+      positive: "+=16",
+      negative: "-=16",
     };
 
     const box = this.getBoxAt(x, y);
@@ -168,17 +197,20 @@ export default class Game extends Phaser.Scene {
       [axis]: directionXY[direction],
       duration: 400,
       onStart: () => {
-        this.player?.anims.play('move', true);
+        this.player?.anims.play("move", true);
         this.moves -= 1;
         this.steps += 1;
         this.movesText?.setText(`${this.moves}`);
         this.stepsText?.setText(`Steps: ${this.steps}`);
         if (this.moves <= 0) {
           this.isGameOver = true;
+          this.bgMusic.pause();
+          //TODO: Add death sound and tint
+          this.player!.setTint(0xff0000);
         }
       },
       onComplete: () => {
-        this.player?.anims.play('idle', true);
+        this.player?.anims.play("idle", true);
       },
       onCompleteScope: this,
     };
@@ -191,6 +223,7 @@ export default class Game extends Phaser.Scene {
       if (!this.checkBoxMovement(box, axis, direction)) {
         return undefined;
       }
+      this.boxDragSound.play();
       this.tweens.add({
         ...baseTween,
         targets: box,
@@ -212,19 +245,19 @@ export default class Game extends Phaser.Scene {
 
     if (this.steps % 2 === 0) {
       for (let sprite of this.spikesAlternating1) {
-        sprite.anims.play('extend');
+        sprite.anims.play("extend");
       }
       for (let sprite of this.spikesAlternating2) {
-        sprite.anims.play('retract');
+        sprite.anims.play("retract");
       }
     }
 
     if (this.steps % 2 === 1) {
       for (let sprite of this.spikesAlternating1) {
-        sprite.anims.play('retract');
+        sprite.anims.play("retract");
       }
       for (let sprite of this.spikesAlternating2) {
-        sprite.anims.play('extend');
+        sprite.anims.play("extend");
       }
     }
 
@@ -251,25 +284,25 @@ export default class Game extends Phaser.Scene {
     axis: string,
     direction: string
   ) {
-    if (axis === 'x' && direction === 'negative') {
+    if (axis === "x" && direction === "negative") {
       if (
         this.hasObjectObstruction(box.getBounds().x - 8, box.getBounds().y + 8)
       ) {
         return false;
       }
-    } else if (axis === 'x' && direction === 'positive') {
+    } else if (axis === "x" && direction === "positive") {
       if (
         this.hasObjectObstruction(box.getBounds().x + 24, box.getBounds().y + 8)
       ) {
         return false;
       }
-    } else if (axis === 'y' && direction === 'negative') {
+    } else if (axis === "y" && direction === "negative") {
       if (
         this.hasObjectObstruction(box.getBounds().x + 8, box.getBounds().y - 8)
       ) {
         return false;
       }
-    } else if (axis === 'y' && direction === 'positive') {
+    } else if (axis === "y" && direction === "positive") {
       if (
         this.hasObjectObstruction(box.getBounds().x + 8, box.getBounds().y + 24)
       ) {
@@ -306,7 +339,7 @@ export default class Game extends Phaser.Scene {
 
   // returns moveable box based on x & y coords
   private getBoxAt(x: number, y: number) {
-    return this.boxes.find(box => {
+    return this.boxes.find((box) => {
       const rect = box.getBounds();
       return rect.contains(x, y);
     });
@@ -314,7 +347,7 @@ export default class Game extends Phaser.Scene {
 
   // returns a barrier of movement based on x & y coords
   private getBarrierAt(x: number, y: number) {
-    return this.barriers.find(barrier => {
+    return this.barriers.find((barrier) => {
       const rect = barrier.getBounds();
       return rect.contains(x, y);
     });
@@ -333,8 +366,8 @@ export default class Game extends Phaser.Scene {
 
   private createPlayerAnimations() {
     this.anims.create({
-      key: 'move',
-      frames: this.anims.generateFrameNumbers('character', {
+      key: "move",
+      frames: this.anims.generateFrameNumbers("character", {
         start: 40,
         end: 43,
       }),
@@ -343,28 +376,28 @@ export default class Game extends Phaser.Scene {
     });
 
     this.anims.create({
-      key: 'idle',
-      frames: [{ key: 'character', frame: 40 }],
+      key: "idle",
+      frames: [{ key: "character", frame: 40 }],
       frameRate: 20,
     });
   }
 
   private getSpikeAt(x: number, y: number) {
-    return this.spikes.find(spikes => {
+    return this.spikes.find((spikes) => {
       const rect = spikes.getBounds();
       return rect.contains(x, y);
     });
   }
 
   private getSpikeAlternating1(x: number, y: number) {
-    return this.spikesAlternating1.find(spikesAlternating1 => {
+    return this.spikesAlternating1.find((spikesAlternating1) => {
       const rect = spikesAlternating1.getBounds();
       return rect.contains(x, y);
     });
   }
 
   private getSpikeAlternating2(x: number, y: number) {
-    return this.spikesAlternating2.find(spikesAlternating2 => {
+    return this.spikesAlternating2.find((spikesAlternating2) => {
       const rect = spikesAlternating2.getBounds();
       return rect.contains(x, y);
     });
@@ -384,16 +417,16 @@ export default class Game extends Phaser.Scene {
 
   private createSpikeAnimations() {
     this.anims.create({
-      key: 'extend',
-      frames: this.anims.generateFrameNumbers('character', {
+      key: "extend",
+      frames: this.anims.generateFrameNumbers("character", {
         start: 353,
         end: 356,
       }),
       frameRate: 10,
     });
     this.anims.create({
-      key: 'retract',
-      frames: this.anims.generateFrameNumbers('character', {
+      key: "retract",
+      frames: this.anims.generateFrameNumbers("character", {
         start: 356,
         end: 353,
       }),
--- client/src/scenes/Intro.ts
+++ client/src/scenes/Intro.ts
@@ -1,46 +1,50 @@
-import Phaser from 'phaser';
-import TextBlink from '../game_components/TextBlink';
+import Phaser from "phaser";
+import TextBlink from "../game_components/TextBlink";
 
 export default class Intro extends Phaser.Scene {
   constructor() {
-    super('intro');
+    super("intro");
   }
 
+  preload() {
+    this.load.audio("audio_scream", "assets/audio/intro_scream.ogg");
+  }
   create() {
     const width = this.scale.width;
     const height = this.scale.height;
-    const title = 'Rise of Lich: Revengeance';
+    const title = "Rise of Lich: Revengeance";
     this.anims.create({
-      key: 'lich-idle',
-      frames: this.anims.generateFrameNumbers('lich', { start: 0, end: 5 }),
+      key: "lich-idle",
+      frames: this.anims.generateFrameNumbers("lich", { start: 0, end: 5 }),
       frameRate: 5,
       repeat: -1,
     });
 
-    this.add.sprite(width * 0.5, height * 0.5, 'lich').play('lich-idle');
+    this.add.sprite(width * 0.5, height * 0.5, "lich").play("lich-idle");
 
     this.add
       .text(width * 0.5, height * 0.25, title, {
         fontSize: 24,
-        fontFamily: 'Metal Mania',
-        color: '#f00',
+        fontFamily: "Metal Mania",
+        color: "#f00",
       })
       .setOrigin(0.5);
 
     const enter = this.add
-      .text(width * 0.5, height * 0.75, 'Press enter to start', {
-        fontFamily: 'Metal Mania',
+      .text(width * 0.5, height * 0.75, "Press enter to start", {
+        fontFamily: "Metal Mania",
         fontSize: 16,
-        color: '#f00',
+        color: "#f00",
       })
       .setOrigin(0.5);
 
     TextBlink.flashElement(this, enter);
 
     this.input.keyboard.once(
-      'keydown-ENTER',
+      "keydown-ENTER",
       () => {
-        this.scene.start('game', { currentLevel: 1, steps: 0 });
+        this.sound.add("audio_scream").play();
+        this.scene.start("game", { currentLevel: 1, steps: 0 });
       },
       this
     );
