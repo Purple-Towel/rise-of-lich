ee0dd5c4b7ae2cd0b1208eadc2d8ce5dbe3e086a
Purple-Towel <angadgrewal31@gmail.com>
2020-06-12 05:56:05 +0000

    modularized tutorials into topics

--- client/src/components/Tutorial.tsx
+++ client/src/components/Tutorial.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useSelectHook } from './hooks/useSelectHook';
 
 const ImageContainer = styled.div`
   display: inline-grid;
@@ -25,29 +26,77 @@ const Paragraph = styled.p`
 `;
 
 const Image = styled.img`
-  width: 30px;
+  width: 50px;
+`;
+
+const Button = styled.button`
+font-size: 28px;
+font-family: 'Metal Mania', cursive;
+background-color: transparent;
+border: 8px inset gold;
+border-radius: 9px;
+:hover {
+  background-color: rgb(224, 171, 24);
+}
 `;
 
 const Tutorial = () => {
+
+  const MOVE = 'Movement';
+  const RESET = 'Reset';
+  const STAMINA = 'Stamina';
+  const CRATES = 'Crates';
+  const LADDERS = 'Ladders';
+  const SPIKES = 'Spikes';
+
+  const selections = [MOVE, RESET, CRATES, LADDERS, STAMINA, SPIKES];
+
+  const Buttons = selections.map((selection, index) => {
+    return (
+      <Button onClick={()=>selectStateIndex(index)}>{selection}</Button>
+    )
+  });
+
+
+  const { selected, selectStateIndex } = useSelectHook(selections);
+
+
   return (
     <Container>
       <Subtitle>Tutorial</Subtitle>
-      <ImageContainer className="grid2x3">
+      {Buttons}
+      <p>{selected}</p>
+      { selected === selections[0] && (<ImageContainer className="grid2x3">
         <Image src="assets/empty.png" alt="empty" />
         <Image src="assets/up_arr.png" alt="up-arrow" />
         <Image src="assets/empty.png" alt="empty" />
         <Image src="assets/left_arr.png" alt="left-arrow" />
         <Image src="assets/down_arr.png" alt="down-arrow" />
         <Image src="assets/right_arr.png" alt="right-arrow" />
-      </ImageContainer>
-      <Paragraph>
-        Movement: You may press any of the above indicated keys to move the
-        character in the corresponding direction. Moving takes Stamina, and once
-        you reach 0 Stamina it will be game over.
-      </Paragraph>
-      <Paragraph>
-        Reset: Press 'r' at any time to reset the current level.
-      </Paragraph>
+      </ImageContainer>)}
+      { selected === selections[0] && (<Paragraph>
+         Pictured above are Arbyn's Wondrous Runes of Movement. Press the indicated runes to cast a spell that moves your character in that direction. (Okay, it's just pressing the arrow keys to move in that direction) Movement takes Stamina.
+      </Paragraph>)}
+      { selected === selections[1] && (<Image src="assets/r_button.png" alt="r-button" />)}
+      { selected === selections[1] && (<Paragraph>
+         Rora's Rune of Resetting works by returing you and all nearby objects back to a moment in time determined arbitrarily. Simply touch the Rune indicated and you and everything else will return to a previous temporal state. (Yeah, this one is just a level reset button. Press R.)
+      </Paragraph>)}
+      { selected === selections[2] && (<Image src="assets/box.png" alt="crates" />)}
+      { selected === selections[2] && (<Paragraph>
+         The Lich has left a lot of crates lying around cluttering up the place. They're heavy and hard to open, but you can shove them around if they are blocking the way, at the cost of some Stamina.
+      </Paragraph>)}
+      { selected === selections[3] && (<Image src="assets/ladder.png" alt="ladder" />)}
+      { selected === selections[3] && (<Paragraph>
+       Why does the Lich have these lying around? He can fly and teleport! Luckily these all lead to the exit and one step closer to your freedom.
+      </Paragraph>)}
+      { selected === selections[4] && (<Image src="assets/hud-icon.png" alt="stamina-icon"/>)}
+      { selected === selections[4] && (<Paragraph>
+         This cursed orb displays how much Stamina you have. Most actions you take will cost Stamina and running out will activate the Lich's curse and cause a game over.
+      </Paragraph>)}
+      { selected === selections[5] && (<Image src="assets/spikes.png" alt="spikes"/>)}
+      { selected === selections[5] && (<Paragraph>
+         These spike traps are somewhat common through the Lich's dungeon. Stepping on them takes twice as much Stamina as usual, so try not to dance on them, as fun as it may seem.
+      </Paragraph>)}
     </Container>
   );
 };
