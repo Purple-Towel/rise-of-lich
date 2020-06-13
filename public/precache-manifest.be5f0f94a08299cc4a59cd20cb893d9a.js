ed06e6c7fa53ce0022e31e74645e32427c2c5fe5
rancewcampbell <rancewcampbell@gmail.com>
2020-06-13 12:51:56 -0600

    save player progress in local storage

---
+++ public/precache-manifest.be5f0f94a08299cc4a59cd20cb893d9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c722554c8c7f5bccb7d9ff23128de2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fe37ecdc6bb3bbbd9a3",
+    "url": "/static/css/main.987b56fc.chunk.css"
+  },
+  {
+    "revision": "1c18df1e4736c124e841",
+    "url": "/static/js/2.407ac36d.chunk.js"
+  },
+  {
+    "revision": "b714726e92dda79eb55fdba59ac9577d",
+    "url": "/static/js/2.407ac36d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fe37ecdc6bb3bbbd9a3",
+    "url": "/static/js/main.452f3329.chunk.js"
+  },
+  {
+    "revision": "c37df48ff32d3d900576",
+    "url": "/static/js/runtime-main.1ef69e4c.js"
+  },
+  {
+    "revision": "1002c4fdf1de89e1fbc6902f6c02f3aa",
+    "url": "/static/media/paperbg.1002c4fd.png"
+  }
+]);
--- src/scenes/Victory.ts
+++ src/scenes/Victory.ts
@@ -9,6 +9,8 @@ export default class Victory extends Phaser.Scene {
     const width = this.scale.width;
     const height = this.scale.height;
 
+    localStorage.setItem('level', '1');
+
     this.add
       .text(width * 0.5, height * 0.25, 'You escaped the Lich King!', {
         fontSize: 24,
