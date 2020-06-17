0971c6e4f59b2debd796c5067194983dddcd317a
rancewcampbell <rancewcampbell@gmail.com>
2020-06-17 00:49:58 -0600

    removed redundant initializing of 'facing' property

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -54,7 +54,6 @@ export default class Game extends Phaser.Scene {
     const { currentLevel, steps } = d;
     this.currentLevel = currentLevel;
     this.steps = steps;
-    this.facing = 'right';
     const map = this.make.tilemap({
       data: this.levels[this.currentLevel - 1].map,
       tileWidth: 16,
